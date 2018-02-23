d506dc3991e8f88198dc78f540bc9d5ca022b50a
Jeffrey Dischler <jdischler.72@gmail.com>
2018-02-23 15:16:39 -0600

    removing unused assets

--- GarbageMain.js
+++ GarbageMain.js
@@ -2,20 +2,22 @@ var Interface = {
 	isMobile : false
 };
 
+var allowStartToggle = true;
+
 $(function() {
 
 	// iOS requires the audio context to be created off of some kind of button push...
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		Interface.isMobile = true;
 		$("body").addClass("Mobile");
 		var element = $("<div>", {"id" : "MobileStart"}).appendTo("body");
-		var button = $("<div>").attr("id", "Button").text("Enter").appendTo(element);
+		var button = $("<div>").attr("id", "Button").text("Activate Audio").appendTo(element);
 		StartAudioContext(Tone.context, button, function(){
 			element.remove();
+			$("body").removeClass("Mobile");
 		});
 	}
-
-	// Loads instruments and wires them out to some fx and the master output
+	// Loads instruments and wires them up to some fx and the master output
 	configureInstruments();
 	
 	Tone.Transport.bpm.value = 140;
@@ -26,20 +28,28 @@ $(function() {
 	  options: {
 		bootstrapColClasses: 'col-md-6',
 		title: 'Title',
+		button: false,
 		editable: false
 	  },
 	  _create: function() { 
-		var div = '<div class="' + this.options.bootstrapColClasses + '">' +
-					'<div class="unselectable section-header">' + this.options.title + '</div>' +
-						'<div class="section-content">' +
-							'<div class="container constrained-container"></div>' +
-						'</div></div>';
+	  	var opt = this.options;
+	  	var button = opt.button ? 
+	  		`<i id="${opt.button.id}" class="col-xs-1 pull-right button fa ${opt.button.fa}"></i>` :
+	  		''
+		var titleClass = this.options.editable ? 'edit-text' : 'not-text'; 
+		var div = `<div class="${this.options.bootstrapColClasses}">` +
+					'<div class="row unselectable section-header">' +
+						`<div class='${titleClass} col-xs-10 header-title'>${opt.title}</div>${button}` +
+					'</div>' +
+					'<div class="section-content">' +
+						'<div class="container constrained-container"></div>' +
+					'</div></div>';
 		$(this.element).append(div); // or replace?
-		// FIXME:
-		if (false && this.options.editable) {
-			$(this.element).on('click', "div.section-header", function() { 	
-				var $lbl = $(this), o = $lbl.text(),
-				$txt = $('<input type="text" class="editable-label-text section-header" value="'+o+'" />');
+		if (this.options.editable) {
+			$(this.element).on('click', "div.header-title", function() { 	
+				allowStartToggle = false;
+				var $lbl = $(this), origText = $lbl.text(),
+				$txt = $('<input type="text" class="col-xs-5 editable-label-text" value="'+origText+'" />');
 				$lbl.replaceWith($txt);
 				$txt.focus();
 				
@@ -47,19 +57,22 @@ $(function() {
 					var no = $(this).val();
 					$lbl.text(no);
 					$txt.replaceWith($lbl);
+					allowStartToggle = true;
 				})
 				.keydown(function(evt ){
-					if(evt.keyCode == 13) {
-						var no = $(this).val();
+					// if return, accept, else if ESC, revert
+					if(evt.keyCode == 13 || evt.keyCode == 27) {
+						var no = evt.keyCode == 13 ? $(this).val() : origText;
 						$lbl.text(no);
 						$txt.replaceWith($lbl);
+						allowStartToggle = true;
 					}
 				});
 			}); 			
 		}
 	  },
 	  setTitle: function(title) {
-		$(this.element).find('.section-header').html(title);
+		$(this.element).find('.header-title').html(title);
 	  },
 	  setContent: function(garbageDiv) {
 		$(this.element).find('.constrained-container').append(garbageDiv);
@@ -79,30 +92,41 @@ $(function() {
 	// Actually init the dealio
 	$( init );	
 	
-	// yeeesh
-	setTimeout(function() {
-		$('#play-start').click(function() {
-			if (Tone.Transport.state != "started") {
-				$('#play-start').addClass('button-active');
-				$('#play-start').removeClass('fa-play-circle-o');
-				$('#play-start').addClass('fa-stop-circle-o');
-				Tone.Transport.start("+0.1");
-				Tone.Transport.schedule(function() {
-					Tone.Transport.stop();
-					$('#play-start').removeClass('button-active');
-					$('#play-start').removeClass('fa-stop-circle-o');
-					$('#play-start').addClass('fa-play-circle-o');
-				}, '18m'); // TODO: get actual song length
-			} 
-			else {
+	function toggleStart() {
+		if (!allowStartToggle) return;
+		if (Tone.Transport.state != "started") {
+			$('#play-start').addClass('button-active');
+			$('#play-start').removeClass('fa-play-circle-o');
+			$('#play-start').addClass('fa-stop-circle-o');
+			Tone.Transport.start("+0.1");
+			Tone.Transport.schedule(function() {
 				Tone.Transport.stop();
 				$('#play-start').removeClass('button-active');
 				$('#play-start').removeClass('fa-stop-circle-o');
 				$('#play-start').addClass('fa-play-circle-o');
-			}
+			}, '18m'); // TODO: get actual song length
+		} 
+		else {
+			Tone.Transport.stop();
+			$('#play-start').removeClass('button-active');
+			$('#play-start').removeClass('fa-stop-circle-o');
+			$('#play-start').addClass('fa-play-circle-o');
+		}
+	}
+	
+	// yeeesh
+	setTimeout(function() {
+		$('#play-start').click(function() {
+			toggleStart();
 		})
 	}, 200);
 	
+	$(window).keypress(function (e) {
+  		if (e.keyCode === 0 || e.keyCode === 32) {
+    		//e.preventDefault()
+			toggleStart();
+  		}
+	})
 	
 })
 
@@ -115,14 +139,17 @@ function init() {
 	//--------------------------------------------------------------------------
 	// The actual Song
 	//----------------------------------------------------------------------
-	$('#song-section').app_Section({title: 'Song <i id="play-start" class="pull-right button fa fa-play-circle-o"></i>',
-		editable: true});
+	$('#song-section').app_Section({title: 'Song Title', button: {id: "play-start", fa: "fa-play-circle-o"},
+		editable: true
+	});
 	var content = '<div id="droppable-area" class="row min-height"></div>'
 	$('#song-section').app_Section('setContent', content);
 	
 	// What the song could be made of...//
 	//----------------------------------------------------------------------
-	$('#section-browser').app_Section({title: 'Section Browser <i id="add-new-section" class="pull-right button fa fa-plus-square-o"></i>'});
+	$('#section-browser').app_Section({title: 'Section Browser', 
+		button:{ id: "add-new-section", fa: "fa-plus-square-o"}
+	});
 	content = '<div id="sb-inner" class="row min-height">' +
 		'<div class="sb-draggable col-xs-1 unselectable song-section green"><div class="unselectable song-section-title">Intro</div></div>' +
 		'<div class="sb-draggable col-xs-1 unselectable song-section blue"><div class="unselectable song-section-title">Verse</div></div>' +
@@ -193,7 +220,8 @@ function init() {
 	
 	// The Detail of a section
 	//----------------------------------------------------------------------
-	$('#section-detail').app_Section({bootstrapColClasses: 'col-md-12', title: 'Section Detail > Intro'});
+	$('#section-detail').app_Section({bootstrapColClasses: 'col-md-12', 
+		title: 'Section Detail > Intro'});
 	
 	// TODO
 	content = '<div class="row song-section-detail green">';
